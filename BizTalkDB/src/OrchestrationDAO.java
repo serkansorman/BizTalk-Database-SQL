e8b805ac1651f57ab3f395a648e96d32b5f6818b
Yusuf Bavaş <yusufbavass@gmail.com>
2018-12-20 16:17:14 +0300

    Update OrchestrationDAO.java

--- BizTalkDB/src/OrchestrationDAO.java
+++ BizTalkDB/src/OrchestrationDAO.java
@@ -1,7 +1,12 @@
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 public class OrchestrationDAO extends DBHandler {
 
@@ -16,12 +21,7 @@ public class OrchestrationDAO extends DBHandler {
         ResultSet rs = preparedStmt.executeQuery();
 
         if(rs.next()) {
-            orchestration.setId(rs.getInt("OrchestrationId"));
-            orchestration.setOwnerID(rs.getInt("OrchestrationOwner"));
-            orchestration.setStatus(rs.getInt("Status"));
-            orchestration.setStartJobID(rs.getInt("StartJobId"));
-            orchestration.setInsertDateTime(rs.getString("InsertDateTime"));
-            orchestration.setUpdateDateTime(rs.getString("UpdateDateTime"));
+            OrchestrationHelper(orchestration,rs);
         }
 
         closePreparedStatement(preparedStmt);
@@ -29,33 +29,31 @@ public class OrchestrationDAO extends DBHandler {
         closeConnection(conn);
 
         return orchestration;
-
     }
 
     // status olayı belirsiz....
-    public Orchestration getOrchestration(int OrchestrationOwner) throws Exception {
+    // o kişiye ait tüm veriler döner ...
+    public Set<Orchestration> getOrchestration(int OrchestrationOwner) throws Exception {
 
         Connection conn = getConnection();
-        Orchestration orchestration = new Orchestration();
+        Set<Orchestration> orchestrations = new HashSet<Orchestration>();
+
         PreparedStatement preparedStmt = conn.prepareStatement("SELECT * FROM Orchestration WHERE OrchestrationOwner=?");
 
         preparedStmt.setInt(1, OrchestrationOwner);
         ResultSet rs = preparedStmt.executeQuery();
 
-        if(rs.next()) {
-            orchestration.setId(rs.getInt("OrchestrationId"));
-            orchestration.setOwnerID(rs.getInt("OrchestrationOwner"));
-            orchestration.setStatus(rs.getInt("Status"));
-            orchestration.setStartJobID(rs.getInt("StartJobId"));
-            orchestration.setInsertDateTime(rs.getString("InsertDateTime"));
-            orchestration.setUpdateDateTime(rs.getString("UpdateDateTime"));
+        while(rs.next()) {
+            Orchestration orchestration = new Orchestration();
+            OrchestrationHelper(orchestration,rs);
+            orchestrations.add(orchestration);
         }
 
         closePreparedStatement(preparedStmt);
         closeResultSet(rs);
         closeConnection(conn);
 
-        return orchestration;
+        return orchestrations;
     }
 
     public boolean insertOrchestration(Orchestration orchestration)  throws Exception {
@@ -75,5 +73,48 @@ public class OrchestrationDAO extends DBHandler {
         closeConnection(conn);
 
         return preparedStmt.execute();
+    }
+
+    public boolean updateOrchestration(int orchestrationid,String columnname,int value)  throws Exception {
+
+        Connection conn = getConnection();
+
+        PreparedStatement preparedStmt = conn.prepareStatement("UPDATE Orchestration SET ? = ? WHERE OrchestrationId = ?");
+
+        preparedStmt.setString(1,columnname);
+        preparedStmt.setInt(2,value);
+        preparedStmt.setInt(3,orchestrationid);
+
+        closePreparedStatement(preparedStmt);
+        closeConnection(conn);
+
+        return preparedStmt.execute();
+    }
+
+    public boolean updateOrchestration(int orchestrationid, String columnname, Date value)  throws Exception {
+
+        Connection conn = getConnection();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+        String date = dateFormat.format(value);
+
+        PreparedStatement preparedStmt = conn.prepareStatement("UPDATE Orchestration SET ? = ? WHERE OrchestrationId = ?");
+
+        preparedStmt.setString(1,columnname);
+        preparedStmt.setString(2,date);
+        preparedStmt.setInt(3,orchestrationid);
+
+        closePreparedStatement(preparedStmt);
+        closeConnection(conn);
+
+        return preparedStmt.execute();
+    }
+
+    private void OrchestrationHelper(Orchestration orchestration, ResultSet rs) throws SQLException, ParseException {
+        orchestration.setId(rs.getInt("OrchestrationId"));
+        orchestration.setOwnerID(rs.getInt("OrchestrationOwner"));
+        orchestration.setStatus(rs.getInt("Status"));
+        orchestration.setStartJobID(rs.getInt("StartJobId"));
+        orchestration.setInsertDateTime(rs.getString("InsertDateTime"));
+        orchestration.setUpdateDateTime(rs.getString("UpdateDateTime"));
     }
 }
